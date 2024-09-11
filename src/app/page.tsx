fd812349b61a75d4bced6043de0a291501667dd7
Gigiox <guillermo.casanova.b@gmail.com>
2024-09-11 20:31:06 -0300

    chore: center the content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Grid from './components/grid/grid';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='flex items-center justify-center min-h-screen'>
+      <div>
+        <h1 className='text-center'>Home</h1>
+        <Grid />
+      </div>
     </div>
   );
 }
